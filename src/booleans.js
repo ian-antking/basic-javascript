1931058f4a10888fc3a05d309909e040369f3fcb
Ian King <ian.antking@gmail.com>
2021-09-20 21:10:01 +0100

    add boolean tests

---
+++ src/booleans.js
@@ -0,0 +1,59 @@
+exports.negate = () => {
+
+};
+
+exports.both = () => {
+
+};
+
+exports.either = () => {
+
+};
+
+exports.none = () => {
+
+};
+
+exports.one = () => {
+
+};
+
+exports.truthiness = () => {
+
+};
+
+exports.isEqual = () => {
+
+};
+
+exports.isGreaterThan = () => {
+
+};
+
+exports.isLessThanOrEqualTo = () => {
+
+};
+
+exports.isOdd = () => {
+
+};
+
+exports.isEven = () => {
+
+};
+
+exports.isSquare = () => {
+
+};
+
+exports.startsWith = () => {
+
+};
+
+exports.containsVowels = () => {
+
+};
+
+exports.isLowerCase = () => {
+
+};
