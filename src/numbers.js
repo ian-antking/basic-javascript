ade29a4b5aa0b8a6d8bbb1fdd0d2b7efa1f7fb5a
Ian King <ian.antking@gmail.com>
2021-09-20 21:03:37 +0100

    add number tests

---
+++ src/numbers.js
@@ -0,0 +1,43 @@
+exports.add = () => {
+
+};
+
+exports.subtract = () => {
+
+};
+
+exports.multiply = () => {
+
+};
+
+exports.divide = () => {
+
+};
+
+exports.power = () => {
+
+};
+
+exports.round = () => {
+
+};
+
+exports.roundUp = () => {
+
+};
+
+exports.roundDown = () => {
+
+};
+
+exports.absolute = () => {
+
+};
+
+exports.quotient = () => {
+
+};
+
+exports.remainder = () => {
+
+};
