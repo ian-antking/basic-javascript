437e9c5471063bb46d1cfaddc0cc6adb72587961
Ian King <ian.antking@gmail.com>
2021-09-20 21:14:25 +0100

    add arrays tests

---
+++ src/arrays.js
@@ -0,0 +1,59 @@
+exports.getNthElement = () => {
+
+};
+
+exports.arrayToCSVString = () => {
+
+};
+
+exports.csvStringToArray = () => {
+
+};
+
+exports.addToArray = () => {
+
+};
+
+exports.addToArray2 = () => {
+
+};
+
+exports.removeNthElement = () => {
+
+};
+
+exports.numbersToStrings = () => {
+
+};
+
+exports.uppercaseWordsInArray = () => {
+
+};
+
+exports.reverseWordsInArray = () => {
+
+};
+
+exports.onlyEven = () => {
+
+};
+
+exports.removeNthElement2 = () => {
+
+};
+
+exports.elementsStartingWithAVowel = () => {
+
+};
+
+exports.removeSpaces = () => {
+
+};
+
+exports.sumNumbers = () => {
+
+};
+
+exports.sortByLastLetter = () => {
+
+};
