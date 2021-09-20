659586f7017d13e71c259b177e2a0f58c032813f
Ian King <ian.antking@gmail.com>
2021-09-20 21:18:06 +0100

    add object tests

---
+++ src/objects.js
@@ -0,0 +1,39 @@
+exports.createPerson = () => {
+
+};
+
+exports.getName = () => {
+
+};
+
+exports.getProperty = () => {
+
+};
+
+exports.hasProperty = () => {
+
+};
+
+exports.isOver65 = () => {
+
+};
+
+exports.getAges = () => {
+
+};
+
+exports.findByName = () => {
+
+};
+
+exports.findHondas = () => {
+
+};
+
+exports.averageAge = () => {
+
+};
+
+exports.createTalkingPerson = () => {
+
+};
